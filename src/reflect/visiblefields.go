d9ee8ea9bd98b1fe38de08b15c04c30c05995d85
sanfeng <sanfeng@ztouch.ai>
2022-07-18 19:10:57 +0800

    使用vim - youdao翻译插件 翻译

--- src/reflect/visiblefields.go
+++ src/reflect/visiblefields.go
@@ -10,9 +10,14 @@ package reflect
 // inside anonymous struct members and unexported fields. They follow
 // the same order found in the struct, with anonymous fields followed
 // immediately by their promoted fields.
+// VisibleFields返回t中所有可见字段，它必须是struct类型
+// 如果字段可以通过FieldByName调用直接访问，则该字段被定义为可见
+// 返回的字段包括匿名结构成员中的字段和未导出的字段
+// 它们遵循与结构中相同的顺序，匿名字段紧跟着它们的提升字段
 //
 // For each element e of the returned slice, the corresponding field
 // can be retrieved from a value v of type t by calling v.FieldByIndex(e.Index).
+// 对于返回片中的每个元素e，可以通过调用v. fieldbyindex (e. index)从类型为t的值v中检索对应的字段
 func VisibleFields(t Type) []StructField {
 	if t == nil {
 		panic("reflect: VisibleFields(nil)")
@@ -30,6 +35,7 @@ func VisibleFields(t Type) []StructField {
 	// Remove all the fields that have been hidden.
 	// Use an in-place removal that avoids copying in
 	// the common case that there are no hidden fields.
+	// 删除所有隐藏的字段 在没有隐藏字段的常见情况下，使用就地删除来避免复制
 	j := 0
 	for i := range w.fields {
 		f := &w.fields[i]
@@ -39,6 +45,7 @@ func VisibleFields(t Type) []StructField {
 		if i != j {
 			// A field has been removed. We need to shuffle
 			// all the subsequent elements up.
+			// 字段已被删除 我们需要把所有的后续元素重新排列
 			w.fields[j] = *f
 		}
 		j++
@@ -58,6 +65,8 @@ type visibleFieldsWalker struct {
 // (this maintains the required ordering).
 // Fields that have been overridden have their
 // Name field cleared.
+// Walk遍历结构类型t中的所有字段，访问索引预序中的字段，并将它们添加到w.fields(这维护了所需的顺序)
+// 已被重写的字段的Name字段将被清除
 func (w *visibleFieldsWalker) walk(t Type) {
 	if w.visiting[t] {
 		return
@@ -74,19 +83,24 @@ func (w *visibleFieldsWalker) walk(t Type) {
 				// cancel one another out. Set the field name
 				// to empty to signify that has happened, and
 				// there's no need to add this field.
+				// 具有相同名称、相同深度的字段会相互抵消
+				// 将字段名称设置为空以表示已经发生，并且不需要添加此字段
 				old.Name = ""
 				add = false
 			} else if len(w.index) < len(old.Index) {
 				// The old field loses because it's deeper than the new one.
+				// 旧油田会损失，因为它比新油田更深
 				old.Name = ""
 			} else {
 				// The old field wins because it's shallower than the new one.
+				// 老油田赢了，因为它比新油田浅
 				add = false
 			}
 		}
 		if add {
 			// Copy the index so that it's not overwritten
 			// by the other appends.
+			// 复制索引，使其不被其他追加覆盖
 			f.Index = append([]int(nil), w.index...)
 			w.byName[f.Name] = len(w.fields)
 			w.fields = append(w.fields, f)
